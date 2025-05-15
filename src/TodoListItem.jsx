eb57dc0252ec57e2306b54f87e133b9778cb7a3f
wonee42 <wonee422005@skuniv.ac.kr>
2025-05-16 03:15:34 +0900

    💄 Update TodoList UI with CSS modules

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,15 +1,21 @@
+import styles from './TodoListItem.module.css';
+
 function TodoListItem({ todo, onToggle, onRemove }) {
   return (
-    <li>
-      <input
-        type="checkbox"
-        checked={todo.checked}
-        onChange={() => onToggle(todo.id)}
-      />
-      <span style={{ textDecoration: todo.checked ? 'line-through' : 'none' }}>
-        {todo.text}
-      </span>
-      <button onClick={() => onRemove(todo.id)}>삭제</button>
+    <li className={styles.item}>
+      <label>
+        <input
+          type="checkbox"
+          checked={todo.checked}
+          onChange={() => onToggle(todo.id)}
+        />
+        <span className={`${styles.text} ${todo.checked ? styles.checked : ''}`}>
+          {todo.text}
+        </span>
+      </label>
+      <button className={styles.delete} onClick={() => onRemove(todo.id)}>
+        삭제
+      </button>
     </li>
   );
 }
